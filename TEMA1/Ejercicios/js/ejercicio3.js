5ea296f1678c965753aee97031b38de7141f1cc1
MARÍA ABASCAL <mariaabascal06@gmail.com>
2025-10-07 12:43:23 +0200

    kjbrgiubg

--- TEMA1/Ejercicios/js/ejercicio3.js
+++ TEMA1/Ejercicios/js/ejercicio3.js
@@ -1,4 +1,5 @@
 function mostrarTexto() {
     const texto = document.getElementById("textoInput").value;
+    //getelementbyid sirve pa mostrarlo 
     document.getElementById("resultado").textContent = texto;
 }
